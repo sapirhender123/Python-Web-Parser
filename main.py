66884d48617518d131005f7c84f27cfeea9fd34d
Sapir Hender <sapirhender@gmail.com>
2022-09-20 18:05:12 +0300

    Sapir: href list working

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import urllib.request
 from bs4 import BeautifulSoup as bs
 import os
 import urllib
-
+'''
 fp = urllib.request.urlopen("https://collaborate.mitre.org/attackics/index.php/Main_Page")
 mybytes = fp.read()
 
@@ -15,4 +15,30 @@ print(mystr)
 urllib.request.urlretrieve("https://collaborate.mitre.org/attackics/index.php/Main_Page", "ATT&CK.html")
 base=os.path.dirname(os.path.abspath('main.py'))
 # open the HTML file
-html=open(os.path.join(base, 'ATT&CK.html'))
+#html=open(os.path.join(base, 'ATT&CK.html'))
+
+
+'''
+urls = []
+file = open('ATT&CK.html', encoding="utf8")
+soup = bs(file, 'html.parser')
+desire_links = soup.findAll('td')
+for link in desire_links:
+    children = link.findChildren()
+    for child in children:
+        print(child['href'])
+links = soup.findAll('a', href=True)
+for link in links:
+    urls.append(link['href'])
+#print(urls)
+
+'''
+
+unordered_list=soup.find("div",
+      {"class":"matrix_container"})
+for child in unordered_list.children:
+    print (child)
+children = unordered_list.findChildren()
+for child in children:
+    print (child)
+    '''
